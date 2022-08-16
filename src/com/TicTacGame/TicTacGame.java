12df933c51657bcc375ab87876b4814902f5e708
Akshayw26 <wangade30@gmail.com>
2022-08-16 19:42:18 +0530

    Added - case two

--- src/com/TicTacGame/TicTacGame.java
+++ src/com/TicTacGame/TicTacGame.java
@@ -1,15 +1,28 @@
 package com.TicTacGame;
 
+import java.util.*;
+
 public class TicTacGame {
     public static void main(String[] args) {
 
-         emptyBoard();
+        emptyBoard();
     }
 
     static char[] board = new char[10];
+    static char playerLetter;
+    static char computerLetter;
+
     private static void emptyBoard() {
         for (int i = 1; i < board.length; i++) {
             board[i] = ' ';
         }
+    }
+
+    static void choice() {
+        System.out.println(" Start the game ");
+        System.out.println("Choose 'X' or 'O'");
+        Scanner scan = new Scanner(System.in);
+        playerLetter = scan.next().charAt(0);
+        computerLetter = (playerLetter == 'X') ? 'O' : 'X';
     }
 }
