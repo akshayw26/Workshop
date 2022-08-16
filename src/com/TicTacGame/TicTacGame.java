76fbac4c252046a977e19da8e7a4037006eea9e8
Akshayw26 <wangade30@gmail.com>
2022-08-16 19:37:28 +0530

    Added - case one

--- src/com/TicTacGame/TicTacGame.java
+++ src/com/TicTacGame/TicTacGame.java
@@ -3,6 +3,13 @@ package com.TicTacGame;
 public class TicTacGame {
     public static void main(String[] args) {
 
+         emptyBoard();
+    }
 
+    static char[] board = new char[10];
+    private static void emptyBoard() {
+        for (int i = 1; i < board.length; i++) {
+            board[i] = ' ';
+        }
     }
 }
